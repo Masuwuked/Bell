18d5cb7ad5ca7ba6012ac1dacedc234b21bb906d
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2024-03-24 20:06:45 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,26 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 
 int main()
 {
-    char *input;
-    while (1)
-    {
-        input = readline("> ");
-        if (!input)
-        {
-            break;
-        }
-        if (strcmp(input, "exit") == 0)
-        {
-            free(input);
-            break;
-        }
-        printf("You entered: %s\n", input);
-        add_history(input);
-        free(input);
-    }
+    system("gnome-terminal");
     return 0;
 }
